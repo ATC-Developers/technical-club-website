d135991159c1076e0d46c695a93a457b82b923d4
Shashank Sharma <shashank.sharma98@gmail.com>
2019-02-27 19:52:27 +0530

    Project initialized

---
+++ backend/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class Home(APIView):
+    """
+    Sample View to show whether the given API is live or not
+    """
+    permission_classes = []
+
+    def get(self, request):
+        return Response({'status': 200})
